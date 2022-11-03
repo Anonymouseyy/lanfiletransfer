962592bfd4bd5ddc731df59a625023c0c626c74e
Anonymouseyy <xieethan2008@gmail.com>
2022-11-02 20:12:56 -0700

    Kinda made file, transfer. Integrate file transfer into main loop

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-import socket, sys, pickle
+import socket, sys, pickle, os
 
 
 # Socket Variables
@@ -7,12 +7,64 @@ HEADER = 64
 PORT = None
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
+END_MESSAGE = 'END'
 SERVER = None
+SIZE = 2048
 ADDR = (SERVER, PORT)
 
 
 def transfer_win(client):
-    pass
+    layout = [[sg.Text('Transfer Files', font=('Helvetica', 32))],
+              [sg.Text('File Destination: ', font=('Helvetica', 20)), sg.Input('', font=('Helvetica', 20), key='dest'), sg.FolderBrowse(font=('Helvetica', 15))],
+              [sg.Text('0/0', font=('Helvetica', 15), key='fraction'), sg.StatusBar('Transfer Status', key='status')],
+              [sg.Button('Exit', font=('Helvetica', 15))]]
+    
+    window = sg.Window('File Transfer (Client)', layout)
+    msg = None
+
+    while True:
+        event, values = window.read()
+        try:
+            client.send(pickle.dumps('CONNECTED'))
+            msg = pickle.loads(client.recv(2048))
+        except socket.error as e:
+            print(e)
+
+        if msg == DISCONNECT_MESSAGE:
+            window.close()
+            client.close()
+            sys.exit()
+        elif msg != 'OK':
+            path = (values['dest'])
+            if not os.DirEntry.isdir(path):
+                client.send(pickle.dumps('NO'))
+                sg.Popup('Select Directory To Transfer To', keep_on_top=True)
+                continue
+            else:
+                client.send(pickle.dumps('OK'))
+
+                write_file = open(f'{path}\\{msg}', 'wb')
+                file_size = pickle.loads(client.recv(2048))
+
+                line = pickle.loads(client.recv(SIZE))
+                count = 0
+
+                while line != END_MESSAGE:
+                    write_file.write(line)
+                    line = pickle.loads(client.recv(SIZE))
+                    count += SIZE
+                    if count > file_size:
+                        count = file_size
+                    window['status'].update(max=file_size, current_count=count)
+                    window['fraction'].update(f'{count}/{file_size}')
+            
+                write_file.close()
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            client.send(pickle.dumps(DISCONNECT_MESSAGE))
+            window.close()
+            client.close()
+            sys.exit()
 
 
 def start():
@@ -23,6 +75,7 @@ def start():
     
     window = sg.Window('File Transfer (Client)', layout)
 
+    client = None
     while True:
         event, values = window.read()
 
@@ -38,12 +91,13 @@ def start():
                     ADDR = SERVER, PORT = values['server_in'], int(values['port_in'])
                     client.connect(ADDR)
                     window.close()
-                    transfer_win(client)
-                    return
                 except:
                     sg.Popup('Check Server And Port Values And Check Server', keep_on_top=True)
+                    client = None
             else:
                 sg.Popup('Check Server And Port Values', keep_on_top=True)
 
+        if client:
+            transfer_win(client)
 
 start()
--- server.py
+++ server.py
@@ -1,20 +1,81 @@
 import PySimpleGUI as sg
-import socket, sys, pickle, pyperclip
+import socket, sys, pickle, pyperclip, os
 
 HEADER = 64
 PORT = 5050
+SIZE = 2048
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
+END_MESSAGE = 'END'
+
+
+def send_file(conn, filepath):
+    pass
+
 
 def handle_client(conn, addr):
     layout = [[sg.Text('Transfer Files', font=('Helvetica', 32))],
-              [sg.Text(f'Server: {SERVER} Port: {PORT}', font=('Helvetica', 20))],
-              [sg.Button('Exit', font=('Helvetica', 15)), sg.Button('Start', font=('Helvetica', 15)),
-               sg.Button('Copy Server', font=('Helvetica', 15), key='server'), sg.Button('Copy Port', font=('Helvetica', 15), key='port')]]
-    print(f'connected to {conn} {addr}')
-    conn.close()
+              [sg.Text('File Select:    ', font=('Helvetica', 20)), sg.Input('', font=('Helvetica', 20), key='file'), sg.FileBrowse(font=('Helvetica', 15))],
+              [sg.Text('Folder Select:', font=('Helvetica', 20)), sg.Input('', font=('Helvetica', 20), key='folder'), sg.FolderBrowse(font=('Helvetica', 15))], 
+              [sg.Text('0/0', font=('Helvetica', 15), key='fraction'), sg.StatusBar('Transfer Status', key='status')],
+              [sg.Button('Exit', font=('Helvetica', 15)), sg.Button('Send File', font=('Helvetica', 15), key='send_file'),
+               sg.Button('Send Folder', font=('Helvetica', 15), key='send_folder')]]
+    
+    window = sg.Window('File Transfer (Server)', layout)
+
+    while True:
+        event, values = window.read()
+        try:
+            conn.send(pickle.dumps('OK'))
+            msg = pickle.loads(conn.recv(SIZE))
+        except socket.error as e:
+            print(e)
+        
+        if msg == DISCONNECT_MESSAGE:
+            window.close()
+            conn.send(pickle.dumps(DISCONNECT_MESSAGE))
+            conn.close()
+            sys.exit()
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            window.close()
+            conn.send(pickle.dumps(DISCONNECT_MESSAGE))
+            conn.close()
+            sys.exit()
+        
+        if event == 'send_file':
+            if not os.DirEntry.is_file(values['file']):
+                sg.Popup('Not Valid File')
+            else:
+                filepath = values['file']
+                file = open(filepath, 'rb')
+                file_size = os.path.getsize(filepath)
+                file_name = os.path.basename(filepath)
+
+                conn.send(pickle.dumps(file_name))
+                msg = pickle.loads(conn.recv(SIZE))
+                if msg == 'NO':
+                    continue
+                else:
+                    conn.send(pickle.dumps(file_size))
+
+                    line = file.read(SIZE)
+                    count = 0
+
+                    while line:
+                        conn.send(line)
+                        line = file.read(SIZE)
+                        count += SIZE
+                        if count > file_size:
+                            count = file_size
+                        window['status'].update(max=file_size, current_count=count)
+                        window['fraction'].update(f'{count}/{file_size}')
+            
+                    conn.send(pickle.dumps(END_MESSAGE))
+
+                    file.close()
 
 
 def start():
@@ -44,7 +105,6 @@ def home():
         if event == 'Start':
             window.close()
             start()
-            return
         
         if event == 'server':
             pyperclip.copy(SERVER)
